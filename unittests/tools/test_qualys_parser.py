shelestvereska/DefectDojo1

Ruff: add and fix COM

unittests/tools/test_qualys_parser.py
@@ -19,7 +19,7 @@ class TestQualysParser(DojoTestCase):
 
     def parse_file_with_no_vuln_has_no_findings(self):
         with open(
-            get_unit_tests_path() + "/scans/qualys/empty.xml"
+            get_unit_tests_path() + "/scans/qualys/empty.xml",
         ) as testfile:
             parser = QualysParser()
             findings = parser.get_findings(testfile, Test())
@@ -36,7 +36,7 @@ class TestQualysParser(DojoTestCase):
 
     def parse_file_with_multiple_vuln_has_multiple_findings(self):
         with open(
-            get_unit_tests_path() + "/scans/qualys/Qualys_Sample_Report.xml"
+            get_unit_tests_path() + "/scans/qualys/Qualys_Sample_Report.xml",
         ) as testfile:
             parser = QualysParser()
             findings = parser.get_findings(testfile, Test())
@@ -47,13 +47,13 @@ class TestQualysParser(DojoTestCase):
 
             finding = findings[0]
             self.assertEqual(
-                finding.title, "QID-6 | DNS Host Name"
+                finding.title, "QID-6 | DNS Host Name",
             )
             self.assertEqual(
-                finding.severity, "Informational"
+                finding.severity, "Informational",
             )
             self.assertEqual(
-                finding.unsaved_endpoints[0].host, "demo13.s02.sjc01.qualys.com"
+                finding.unsaved_endpoints[0].host, "demo13.s02.sjc01.qualys.com",
             )
             for finding in findings:
                 if finding.unsaved_endpoints[0].host == "demo14.s02.sjc01.qualys.com" and finding.title == "QID-370876 | AMD Processors Multiple Security Vulnerabilities (RYZENFALL/MASTERKEY/CHIMERA-FW/FALLOUT)":
@@ -62,15 +62,15 @@ class TestQualysParser(DojoTestCase):
                     finding_cvssv3_vector = finding
             self.assertEqual(
                 # CVSS_FINAL is defined without a cvssv3 vector
-                finding_cvssv3_score.cvssv3, None
+                finding_cvssv3_score.cvssv3, None,
             )
             self.assertEqual(
-                finding_cvssv3_score.severity, "High"
+                finding_cvssv3_score.severity, "High",
             )
             self.assertEqual(finding_cvssv3_vector.cvssv3,
                             "CVSS:3.0/AV:N/AC:H/PR:N/UI:N/S:C/C:H/I:H/A:H")
             self.assertEqual(
-                finding_cvssv3_vector.severity, "Critical"
+                finding_cvssv3_vector.severity, "Critical",
             )
             return finding
 
@@ -83,7 +83,7 @@ class TestQualysParser(DojoTestCase):
 
     def parse_file_with_no_vuln_has_no_findings_csv(self):
         with open(
-            get_unit_tests_path() + "/scans/qualys/empty.csv"
+            get_unit_tests_path() + "/scans/qualys/empty.csv",
         ) as testfile:
             parser = QualysParser()
             findings = parser.get_findings(testfile, Test())
@@ -100,7 +100,7 @@ class TestQualysParser(DojoTestCase):
 
     def parse_file_with_multiple_vuln_has_multiple_findings_csv(self):
         with open(
-            get_unit_tests_path() + "/scans/qualys/Qualys_Sample_Report.csv"
+            get_unit_tests_path() + "/scans/qualys/Qualys_Sample_Report.csv",
         ) as testfile:
             parser = QualysParser()
             findings = parser.get_findings(testfile, Test())
@@ -114,30 +114,30 @@ class TestQualysParser(DojoTestCase):
                 finding.title,
                 "QID-105971 | EOL/Obsolete Software: Microsoft ASP.NET 1.0 Detected")
             self.assertEqual(
-                finding.severity, "Critical"
+                finding.severity, "Critical",
             )
             self.assertEqual(
-                finding.unsaved_endpoints[0].host, "ip-10-98-57-180.eu-west-1.compute.internal"
+                finding.unsaved_endpoints[0].host, "ip-10-98-57-180.eu-west-1.compute.internal",
             )
 
             for finding in findings:
                 if finding.unsaved_endpoints[0].host == "ip-10-98-57-180.eu-west-1.compute.internal" and finding.title == "QID-105971 | EOL/Obsolete Software: Microsoft ASP.NET 1.0 Detected":
 
                     self.assertEqual(
-                        finding.severity, "Critical"
+                        finding.severity, "Critical",
                     )
                     self.assertEqual(
                         finding.cvssv3,
                         "CVSS:3.0/AV:N/AC:H/PR:N/UI:N/S:U/C:H/I:H/A:H/E:U/RL:U/RC:C")
                     self.assertEqual(
-                        finding.severity, "Critical"
+                        finding.severity, "Critical",
                     )
 
             return findings[0]
 
     def test_parse_file_monthly_pci_issue6932(self):
         with open(
-            get_unit_tests_path() + "/scans/qualys/monthly_pci_issue6932.csv"
+            get_unit_tests_path() + "/scans/qualys/monthly_pci_issue6932.csv",
         ) as testfile:
             parser = QualysParser()
             findings = parser.get_findings(testfile, Test())
@@ -145,7 +145,7 @@ class TestQualysParser(DojoTestCase):
 
     def test_parse_file_with_cvss_values_and_scores(self):
         with open(
-            get_unit_tests_path() + "/scans/qualys/Qualys_Sample_Report.xml"
+            get_unit_tests_path() + "/scans/qualys/Qualys_Sample_Report.xml",
         ) as testfile:
             parser = QualysParser()
             findings = parser.get_findings(testfile, Test())
@@ -159,22 +159,22 @@ class TestQualysParser(DojoTestCase):
             # The CVSS Vector is not used from the Knowledgebase
             self.assertEqual(
                 # CVSS_FINAL is defined without a cvssv3 vector
-                finding_cvssv3_score.cvssv3, None
+                finding_cvssv3_score.cvssv3, None,
             )
             # Nevertheless the CVSSv3 Score should be set
             self.assertEqual(
-                finding_cvssv3_score.cvssv3_score, 8.2
+                finding_cvssv3_score.cvssv3_score, 8.2,
             )
             # If no cvss information is present in detection and not in knowledgebase values should be empty
             self.assertEqual(
-                finding_no_cvssv3.cvssv3, None
+                finding_no_cvssv3.cvssv3, None,
             )
             self.assertEqual(
-                finding_no_cvssv3.cvssv3_score, None
+                finding_no_cvssv3.cvssv3_score, None,
             )
             # No CVSS Values available in detection and it uses the knowledgebase then
             self.assertEqual(finding_no_cvssv3_at_detection.cvssv3,
                             "CVSS:3.0/AV:N/AC:H/PR:N/UI:N/S:C/C:H/I:H/A:H")
             self.assertEqual(
-                finding_no_cvssv3_at_detection.cvssv3_score, 9.0
+                finding_no_cvssv3_at_detection.cvssv3_score, 9.0,
             )

graphyhiren/dojo

unittests/tools/test_qualys_parser.py
@@ -19,7 +19,7 @@ class TestQualysParser(DojoTestCase):
 
     def parse_file_with_no_vuln_has_no_findings(self):
         with open(
-            get_unit_tests_path() + "/scans/qualys/empty.xml"
+            get_unit_tests_path() + "/scans/qualys/empty.xml",
         ) as testfile:
             parser = QualysParser()
             findings = parser.get_findings(testfile, Test())
@@ -36,7 +36,7 @@ class TestQualysParser(DojoTestCase):
 
     def parse_file_with_multiple_vuln_has_multiple_findings(self):
         with open(
-            get_unit_tests_path() + "/scans/qualys/Qualys_Sample_Report.xml"
+            get_unit_tests_path() + "/scans/qualys/Qualys_Sample_Report.xml",
         ) as testfile:
             parser = QualysParser()
             findings = parser.get_findings(testfile, Test())
@@ -47,13 +47,13 @@ class TestQualysParser(DojoTestCase):
 
             finding = findings[0]
             self.assertEqual(
-                finding.title, "QID-6 | DNS Host Name"
+                finding.title, "QID-6 | DNS Host Name",
             )
             self.assertEqual(
-                finding.severity, "Informational"
+                finding.severity, "Informational",
             )
             self.assertEqual(
-                finding.unsaved_endpoints[0].host, "demo13.s02.sjc01.qualys.com"
+                finding.unsaved_endpoints[0].host, "demo13.s02.sjc01.qualys.com",
             )
             for finding in findings:
                 if finding.unsaved_endpoints[0].host == "demo14.s02.sjc01.qualys.com" and finding.title == "QID-370876 | AMD Processors Multiple Security Vulnerabilities (RYZENFALL/MASTERKEY/CHIMERA-FW/FALLOUT)":
@@ -62,15 +62,15 @@ class TestQualysParser(DojoTestCase):
                     finding_cvssv3_vector = finding
             self.assertEqual(
                 # CVSS_FINAL is defined without a cvssv3 vector
-                finding_cvssv3_score.cvssv3, None
+                finding_cvssv3_score.cvssv3, None,
             )
             self.assertEqual(
-                finding_cvssv3_score.severity, "High"
+                finding_cvssv3_score.severity, "High",
             )
             self.assertEqual(finding_cvssv3_vector.cvssv3,
                             "CVSS:3.0/AV:N/AC:H/PR:N/UI:N/S:C/C:H/I:H/A:H")
             self.assertEqual(
-                finding_cvssv3_vector.severity, "High"
+                finding_cvssv3_vector.severity, "High",
             )
             return finding
 
@@ -83,7 +83,7 @@ class TestQualysParser(DojoTestCase):
 
     def parse_file_with_no_vuln_has_no_findings_csv(self):
         with open(
-            get_unit_tests_path() + "/scans/qualys/empty.csv"
+            get_unit_tests_path() + "/scans/qualys/empty.csv",
         ) as testfile:
             parser = QualysParser()
             findings = parser.get_findings(testfile, Test())
@@ -100,7 +100,7 @@ class TestQualysParser(DojoTestCase):
 
     def parse_file_with_multiple_vuln_has_multiple_findings_csv(self):
         with open(
-            get_unit_tests_path() + "/scans/qualys/Qualys_Sample_Report.csv"
+            get_unit_tests_path() + "/scans/qualys/Qualys_Sample_Report.csv",
         ) as testfile:
             parser = QualysParser()
             findings = parser.get_findings(testfile, Test())
@@ -114,30 +114,30 @@ class TestQualysParser(DojoTestCase):
                 finding.title,
                 "QID-105971 | EOL/Obsolete Software: Microsoft ASP.NET 1.0 Detected")
             self.assertEqual(
-                finding.severity, "Critical"
+                finding.severity, "Critical",
             )
             self.assertEqual(
-                finding.unsaved_endpoints[0].host, "ip-10-98-57-180.eu-west-1.compute.internal"
+                finding.unsaved_endpoints[0].host, "ip-10-98-57-180.eu-west-1.compute.internal",
             )
 
             for finding in findings:
                 if finding.unsaved_endpoints[0].host == "ip-10-98-57-180.eu-west-1.compute.internal" and finding.title == "QID-105971 | EOL/Obsolete Software: Microsoft ASP.NET 1.0 Detected":
 
                     self.assertEqual(
-                        finding.severity, "Critical"
+                        finding.severity, "Critical",
                     )
                     self.assertEqual(
                         finding.cvssv3,
                         "CVSS:3.0/AV:N/AC:H/PR:N/UI:N/S:U/C:H/I:H/A:H/E:U/RL:U/RC:C")
                     self.assertEqual(
-                        finding.severity, "Critical"
+                        finding.severity, "Critical",
                     )
 
             return findings[0]
 
     def test_parse_file_monthly_pci_issue6932(self):
         with open(
-            get_unit_tests_path() + "/scans/qualys/monthly_pci_issue6932.csv"
+            get_unit_tests_path() + "/scans/qualys/monthly_pci_issue6932.csv",
         ) as testfile:
             parser = QualysParser()
             findings = parser.get_findings(testfile, Test())
@@ -145,7 +145,7 @@ class TestQualysParser(DojoTestCase):
 
     def test_parse_file_with_cvss_values_and_scores(self):
         with open(
-            get_unit_tests_path() + "/scans/qualys/Qualys_Sample_Report.xml"
+            get_unit_tests_path() + "/scans/qualys/Qualys_Sample_Report.xml",
         ) as testfile:
             parser = QualysParser()
             findings = parser.get_findings(testfile, Test())
@@ -159,24 +159,24 @@ class TestQualysParser(DojoTestCase):
             # The CVSS Vector is not used from the Knowledgebase
             self.assertEqual(
                 # CVSS_FINAL is defined without a cvssv3 vector
-                finding_cvssv3_score.cvssv3, None
+                finding_cvssv3_score.cvssv3, None,
             )
             # Nevertheless the CVSSv3 Score should be set
             self.assertEqual(
-                finding_cvssv3_score.cvssv3_score, 8.2
+                finding_cvssv3_score.cvssv3_score, 8.2,
             )
             # If no cvss information is present in detection and not in knowledgebase values should be empty
             self.assertEqual(
-                finding_no_cvssv3.cvssv3, None
+                finding_no_cvssv3.cvssv3, None,
             )
             self.assertEqual(
-                finding_no_cvssv3.cvssv3_score, None
+                finding_no_cvssv3.cvssv3_score, None,
             )
             # No CVSS Values available in detection and it uses the knowledgebase then
             self.assertEqual(finding_no_cvssv3_at_detection.cvssv3,
                             "CVSS:3.0/AV:N/AC:H/PR:N/UI:N/S:C/C:H/I:H/A:H")
             self.assertEqual(
-                finding_no_cvssv3_at_detection.cvssv3_score, 9.0
+                finding_no_cvssv3_at_detection.cvssv3_score, 9.0,
             )
 
     def test_get_severity_legacy(self):